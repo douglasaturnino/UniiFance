13040107d9a44175b361af3bc7dd7af8da6ddb1c
Douglas Saturnino <douglasaturnino@gmail.com>
2024-02-03 18:29:06 -0300

    Feito o docstring do data_transformation.py

--- projeto/src/data/data_transformation.py
+++ projeto/src/data/data_transformation.py
@@ -12,11 +12,39 @@ from utils.utils import load_config_file
 logger = structlog.getLogger()
 
 class DataTransformation:
+    """
+    Classe para realizar transformações nos dados, incluindo divisão em conjuntos de treino e teste.
+
+    Esta classe oferece funcionalidades para manipulação de dados, incluindo a divisão do conjunto de dados em
+    subconjuntos de treino e teste.
+
+    Attributes:
+        dataframe (pd.DataFrame): O DataFrame contendo os dados a serem transformados.
+        target_name (str): O nome da coluna alvo no DataFrame.
+    """
     def __init__(self, dataframe: pd.DataFrame):
+        """
+        Inicializa uma instância da classe DataTransformation.
+
+        Args:
+            dataframe (pd.DataFrame): O DataFrame contendo os dados a serem transformados.
+        """
         self.dataframe = dataframe 
         self.target_name = load_config_file().get('target_name')
         
     def train_test_spliting(self):
+        """
+        Divide o conjunto de dados em subconjuntos de treino e teste.
+
+        Retorna os conjuntos de treino e teste para recursos (X) e alvos (y).
+
+        Returns:
+            tuple: Uma tupla contendo quatro elementos: X_train, X_valid, y_train, y_valid.
+                X_train (pd.DataFrame): O conjunto de recursos de treino.
+                X_valid (pd.DataFrame): O conjunto de recursos de teste.
+                y_train (pd.Series): O conjunto de alvos de treino.
+                y_valid (pd.Series): O conjunto de alvos de teste.
+        """
         X = self.dataframe.drop(self.target_name, axis=1)
         y = self.dataframe[self.target_name]
         

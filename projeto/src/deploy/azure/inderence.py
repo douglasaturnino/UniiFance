9f5b66ccd669d8a8b67c27bb8147b22d41a9c672
Douglas Saturnino <douglasaturnino@gmail.com>
2024-02-08 09:43:23 -0300

    Criado o inderence.py no azure

---
+++ projeto/src/deploy/azure/inderence.py
@@ -0,0 +1,28 @@
+import json
+
+import pandas as pd
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+
+workspace_name = "prob-loan-ws"
+workspace_location = "East US"
+resource_group = "azure-mlops"
+subscription_id = "SUBSCRIPTION_ID"
+endpoint_name = "prob-loan-endpoint"
+
+ml_client = MLClient(
+    DefaultAzureCredential(), subscription_id, resource_group, workspace_name
+)
+
+df_test = pd.read_csv("projeto/data/raw/test.csv")
+data = {"input_data": df_test.iloc[[0]].to_dict(orient="split")}
+print(data)
+
+with open("file.json", "w") as f:
+    json.dump(data, f)
+
+response = ml_client.online_endpoints.invoke(
+    endpoint_name=endpoint_name, request_file="file.json"
+)
+
+print(response)
